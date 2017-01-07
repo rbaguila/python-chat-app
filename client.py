b628ae04a741a948e0b98a1d676655d8cd2fea38
Roinand Aguila <rbaguila1@up.edu.ph>
2017-01-07 14:14:16 +0800

    Added a user interface using python-tkinter

--- client.py
+++ client.py
@@ -1,35 +1,108 @@
-# Roinand Aguila
-# IT 238 Exer 1
-# chat_client.py
-# Reference: NETWORK PROGRAMMING II - CHAT SERVER & CLIENT (http://www.bogotobogo.com/python/python_network_programming_tcp_server_client_chat_server_chat_client_select.php)
+'''
+Roinand Aguila
+IT 238 Exercise 2
+'''
 
+import thread
+from Tkinter import *
+from socket import *
+import re
 import sys
-import socket
 import select
- 
-def chat_client():
-    if(len(sys.argv) < 3) :
-        print 'Usage : python chat_client.py hostname port username'
-        sys.exit()
 
-    host = sys.argv[1]
-    port = int(sys.argv[2])
-    username = str(sys.argv[3])
-     
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# Connection Variables
+WindowTitle = 'IT238 Chat v0.1 - Client'
+HOST = ""
+PORT = 9009
+s = socket(AF_INET, SOCK_STREAM)
+
+
+
+def ConnectAction():
+    thread.start_new_thread(ReceiveData,())
+
+def LeaveAction():
+    s.close()
+    exit()
+
+# Keyboard Events
+def PressAction(event):
+    EntryBox.config(state=NORMAL)
+    ClickAction()
+def DisableEntry(event):
+    EntryBox.config(state=DISABLED)
+
+# Chat Functions
+def FilteredMessage(EntryText):
+    """
+    Filter out all useless white lines at the end of a string,
+    returns a new, beautifully filtered string.
+    """
+    EndFiltered = ''
+    for i in range(len(EntryText)-1,-1,-1):
+        if EntryText[i]!='\n':
+            EndFiltered = EntryText[0:i+1]
+            break
+    for i in range(0,len(EndFiltered), 1):
+            if EndFiltered[i] != "\n":
+                    return EndFiltered[i:]+'\n'
+    return ''
+    
+def LoadConnectionInfo(ChatLog, EntryText):
+    if EntryText != '':
+        ChatLog.config(state=NORMAL)
+        if ChatLog.index('end') != None:
+            ChatLog.insert(END, EntryText+'\n')
+            ChatLog.config(state=DISABLED)
+            ChatLog.yview(END)
+
+def LoadMyEntry(ChatLog, EntryText):
+    if EntryText != '':
+        ChatLog.config(state=NORMAL)
+        if ChatLog.index('end') != None:
+            LineNumber = float(ChatLog.index('end'))-1.0
+            Username = UserNameBox.get("1.0", "end-1c")
+            ChatLog.insert(END, Username +": "+ EntryText)
+            ChatLog.tag_add(Username, LineNumber, LineNumber+0.4)
+            ChatLog.tag_config(Username, foreground="#3b5998", font=("Arial", 12, "bold"))
+            ChatLog.config(state=DISABLED)
+            ChatLog.yview(END)
+
+
+def LoadOtherEntry(ChatLog, EntryText):
+    if EntryText != '':
+        ChatLog.config(state=NORMAL)
+        if ChatLog.index('end') != None:
+            try:
+                LineNumber = float(ChatLog.index('end'))-1.0
+            except:
+                pass
+            ChatLog.insert(END, EntryText)
+            ChatLog.tag_add(":", LineNumber, LineNumber+0.6)
+            ChatLog.tag_config(":", foreground="#04B404", font=("Arial", 12, "bold"))
+            ChatLog.config(state=DISABLED)
+            ChatLog.yview(END)
+
+# Connection
+
+def ReceiveData():
     s.settimeout(2)
-     
-    # connect to remote host
-    try :
-        s.connect((host, port))
-    except :
-        print 'Unable to connect'
-        sys.exit()
-     
-    print 'You are now connected to remote host. You can start sending messages'
-    sys.stdout.write('[' + username + '] '); sys.stdout.flush()
-     
+    try:
+        
+        HOST = IPBox.get("1.0", "end-1c")
+        PORT = int(PortBox.get("1.0", "end-1c"))
+
+        print HOST
+        print PORT
+        s.connect((HOST, PORT))
+        LoadConnectionInfo(ChatLog, '[ Succesfully connected ]\n---------------------------------------------------------------')
+    except:
+        LoadConnectionInfo(ChatLog, '[ Unable to connect ]')
+        return
+    
     while 1:
+
         socket_list = [sys.stdin, s]
          
         # Get the list sockets which are readable
@@ -38,25 +111,105 @@ def chat_client():
         for sock in ready_to_read:             
             if sock == s:
                 # incoming message from remote server, s
-                data = sock.recv(4096)
+                data = sock.recv(1024)
                 if not data :
-                    print '\nDisconnected from chat server'
+                    LoadConnectionInfo(ChatLog, '\n [ Disconnected from chat server ]\n')
+                    # print '\nDisconnected from chat server'
                     sys.exit()
                 else :
-                    #print data
-                    sys.stdout.write(data)
-                    sys.stdout.write('[' + username + '] '); sys.stdout.flush()     
+                    #print data   
+                    LoadOtherEntry(ChatLog, data) 
+    s.close()
+
+
+# Mouse Events
+def ClickAction():
+
+    #Write message to chat window
+    EntryText = FilteredMessage(EntryBox.get("0.0",END))
+    LoadMyEntry(ChatLog, EntryText)
+
+    #Scroll to the bottom of chat windows
+    ChatLog.yview(END)
+
+    #Erace previous message in Entry Box
+    EntryBox.delete("0.0",END)
             
-            else :
-                # user entered a message
-                msg = sys.stdin.readline()
-                s.send(msg)
-                sys.stdout.write('[' + username + '] '); sys.stdout.flush() 
+    #Send my mesage to all others
+    username = UserNameBox.get("1.0", "end-1c")
+    s.sendall(username+': '+EntryText)
 
-if __name__ == "__main__":
+# GUI
 
-    #sys.exit(chat_client())
-    try:
-        chat_client()
-    except:
-        print '\nChat Client Stopped by Ctrl + C'
+#Create a window
+base = Tk()
+base.title(WindowTitle)
+base.geometry("400x500")
+base.resizable(width=FALSE, height=FALSE)
+
+#Create a Chat window
+ChatLog = Text(base, bd=0, bg="white", height="8", width="50", font="Arial",)
+ChatLog.insert(END, "Connecting to your partner..\n")
+ChatLog.config(state=DISABLED)
+
+#Bind a scrollbar to the Chat window
+scrollbar = Scrollbar(base, command=ChatLog.yview, cursor="heart")
+ChatLog['yscrollcommand'] = scrollbar.set
+
+
+
+#Create the Button to send message
+LeaveButton = Button(base, font=30, text="Leave", width="10", height=2,
+                    bd=0, fg="white", justify=CENTER, bg="#3b5998", activebackground="#4e69a2",
+                    command=LeaveAction)
+
+#Create the box to enter message
+EntryBox = Text(base, bd=0, bg="white",width="29", height="5", font="Arial")
+EntryBox.bind("<Return>", DisableEntry)
+EntryBox.bind("<KeyRelease-Return>", PressAction)
+
+#Create entry box for port
+PortBox = Text(base, bd=0, bg="white",width="6", height="1", font="Arial")
+PortBox.insert(END, "PORT")
+PortBox.bind("<Return>", DisableEntry)
+PortBox.bind("<KeyRelease-Return>", PressAction)
+
+#Create entry box for ip
+IPBox = Text(base, bd=0, bg="white",width="11", height="1", font="Arial")
+IPBox.insert(END, "IP ADDRESS")
+IPBox.bind("<Return>", DisableEntry)
+IPBox.bind("<KeyRelease-Return>", PressAction)
+
+#Create entry box for username
+UserNameBox = Text(base, bd=0, bg="white",width="13", height="1", font="Arial")
+UserNameBox.insert(END, "USER NAME")
+UserNameBox.bind("<Return>", DisableEntry)
+UserNameBox.bind("<KeyRelease-Return>", PressAction)
+
+#Create the Button to send message
+username = UserNameBox.get("1.0", "end-1c")
+SendButton = Button(base, font=30, text="Send", width="10", height=2,
+                    bd=0, fg="white", justify=CENTER, bg="#3b5998", activebackground="#4e69a2",
+                    command=ClickAction)
+
+ConnectButton = Button(base, font=30, text="Connect", width="5", height=2,
+                    bd=0, fg="white", justify=CENTER, bg="#3b5998", activebackground="#4e69a2",
+                    command=ConnectAction)
+
+# Place all components on the screen
+IPBox.place(x=132,y=6, height=22)
+PortBox.place(x=240,y=6, height=22)
+UserNameBox.place(x=6,y=6, height=22)
+scrollbar.place(x=376,y=35, height=357)
+ChatLog.place(x=6,y=35, height=357, width=370)
+EntryBox.place(x=6, y=401, height=90, width=265)
+SendButton.place(x=271, y=401, height=45)
+LeaveButton.place(x=271, y=446, height=45)
+ConnectButton.place(x=310, y=6, height=25)
+
+
+
+
+
+
+base.mainloop()
--- server.py
+++ server.py
@@ -1,89 +1,216 @@
-# Roinand Aguila
-# IT 238 Exer 1
-# chat_server.py
-# Reference: NETWORK PROGRAMMING II - CHAT SERVER & CLIENT (http://www.bogotobogo.com/python/python_network_programming_tcp_server_client_chat_server_chat_client_select.php)
+'''
+Roinand Aguila
+IT 238 Exercise 2
+'''
 
- 
-import sys
-import socket
+import thread
+from Tkinter import *
+from socket import *
+import re
 import select
 
-HOST = '' 
+
+
+#Initiate socket and bind port to Server
+WindowTitle = 'IT238 Chat v0.1 - Server'
+
 SOCKET_LIST = []
-RECV_BUFFER = 4096 
-PORT = 9009
+RECV_BUFFER = 1024 
 
-def chat_server():
 
-    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    server_socket.bind((HOST, PORT))
-    server_socket.listen(10)
- 
+s = socket(AF_INET, SOCK_STREAM)
+s.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
+
+
+
+def ClickAction():
+    
+    thread.start_new_thread(GetConnected,())
+    
+
+    
+
+def PressAction(event):
+    PortBox.config(state=NORMAL)
+    ClickAction()
+def DisableEntry(event):
+    PortBox.config(state=DISABLED)
+
+
+# Chat Functions
+
+def FilteredMessage(EntryText):
+    """
+    Filter out all useless white lines at the end of a string,
+    returns a new, beautifully filtered string.
+    """
+    EndFiltered = ''
+    for i in range(len(EntryText)-1,-1,-1):
+        if EntryText[i]!='\n':
+            EndFiltered = EntryText[0:i+1]
+            break
+    for i in range(0,len(EndFiltered), 1):
+            if EndFiltered[i] != "\n":
+                    return EndFiltered[i:]+'\n'
+    return ''
+    
+def LoadConnectionInfo(ChatLog, EntryText):
+    if EntryText != '':
+        ChatLog.config(state=NORMAL)
+        if ChatLog.index('end') != None:
+            ChatLog.insert(END, EntryText+'\n')
+            ChatLog.config(state=DISABLED)
+            ChatLog.yview(END)
+
+def LoadMyEntry(ChatLog, EntryText):
+    if EntryText != '':
+        ChatLog.config(state=NORMAL)
+        if ChatLog.index('end') != None:
+            LineNumber = float(ChatLog.index('end'))-1.0
+            Username = UserNameBox.get("1.0", "end-1c")
+            ChatLog.insert(END, Username +": "+ EntryText)
+            ChatLog.tag_add(Username, LineNumber, LineNumber+0.4)
+            ChatLog.tag_config(Username, foreground="#3b5998", font=("Arial", 12, "bold"))
+            ChatLog.config(state=DISABLED)
+            ChatLog.yview(END)
+
+def LoadOtherEntry(ChatLog, EntryText):
+    if EntryText != '':
+        ChatLog.config(state=NORMAL)
+        if ChatLog.index('end') != None:
+            try:
+                LineNumber = float(ChatLog.index('end'))-1.0
+            except:
+                pass
+            ChatLog.insert(END, EntryText)
+            ChatLog.tag_add(":", LineNumber, LineNumber+0.6)
+            ChatLog.tag_config(":", foreground="#04B404", font=("Arial", 12, "bold"))
+            ChatLog.config(state=DISABLED)
+            ChatLog.yview(END)
+
+
+# GUI
+
+#Create a window
+base = Tk()
+base.title(WindowTitle)
+base.geometry("400x500")
+base.resizable(width=FALSE, height=FALSE)
+
+#Create a Chat window
+ChatLog = Text(base, bd=0, bg="white", height="8", width="50", font="Arial",)
+ChatLog.insert(END, "Waiting for Client Connections...\n")
+ChatLog.config(state=DISABLED)
+
+#Bind a scrollbar to the Chat window
+scrollbar = Scrollbar(base, command=ChatLog.yview, cursor="heart")
+ChatLog['yscrollcommand'] = scrollbar.set
+
+#Create the Button to send message
+SendButton = Button(base, font=30, text="Send", width="12", height=5,
+                    bd=0, bg="#FFBF00", activebackground="#FACC2E",
+                    command=ClickAction)
+
+#Create the box to enter message
+EntryBox = Text(base, bd=0, bg="white",width="29", height="5", font="Arial")
+EntryBox.bind("<Return>", DisableEntry)
+EntryBox.bind("<KeyRelease-Return>", PressAction)
+
+#Create entry box for port
+PortBox = Text(base, bd=0, bg="white",width="10", height="1", font="Arial")
+PortBox.insert(END, "PORT")
+PortBox.bind("<Return>", DisableEntry)
+PortBox.bind("<KeyRelease-Return>", PressAction)
+
+StartButton = Button(base, font=30, text="Start Server", width="10", height=2,
+                    bd=0, fg="white", justify=CENTER, bg="#3b5998", activebackground="#4e69a2",
+                    command=ClickAction)
+
+
+#Place all components on the screen
+PortBox.place(x=100,y=6, height=22)
+scrollbar.place(x=376,y=35, height=447)
+ChatLog.place(x=6,y=35, height=447, width=370)
+StartButton.place(x=200, y=6, height=25)
+
+
+
+#---------------------------------------------------#
+#----------------CONNECTION MANAGEMENT--------------#
+#---------------------------------------------------#
+def GetConnected():
+    PORT = 9009
+    HOST = '' 
+    print "Port: %s"%(PortBox.get("1.0", "end-1c"))
+    PORT = int(PortBox.get("1.0", "end-1c"))
+    s.bind((HOST, PORT))
+    s.listen(10)
     # add server socket object to the list of readable connections
-    SOCKET_LIST.append(server_socket)
- 
-    print "Chat server started on port " + str(PORT)
- 
-    while 1:
+    SOCKET_LIST.append(s)
 
+    
+    while 1:
         # get the list sockets which are ready to be read through select
         # 4th arg, time_out  = 0 : poll and never block
         ready_to_read,ready_to_write,in_error = select.select(SOCKET_LIST,[],[],0)
-      
+
+
+
         for sock in ready_to_read:
             # a new connection request recieved
-            if sock == server_socket: 
-                sockfd, addr = server_socket.accept()
+            if sock == s: 
+                sockfd, addr = s.accept()
                 SOCKET_LIST.append(sockfd)
-                print "Client (%s, %s) connected" % addr
-                 
-                broadcast(server_socket, sockfd, "[%s:%s] entered our chatting room\n" % addr)
-             
+                # print "Client (%s, %s) connected" % addr
+                LoadConnectionInfo(ChatLog, '\n Connected with: ' + str(addr) + '\n---------------------------------------------------------------')
+                
+                broadcast(s, sockfd, "[%s:%s] entered our chatting room\n" % addr)
+
             # a message from a client, not a new connection
             else:
                 # process data recieved from client, 
                 try:
                     # receiving data from the socket.
-                    data = sock.recv(RECV_BUFFER)
+                    data = sock.recv(1024)
                     if data:
                         # there is something in the socket
-                        broadcast(server_socket, sock, "\r" + '[' + str(sock.getpeername()) + '] ' + data)  
+                        broadcast(s, sock, data)
+                        
+                        LoadOtherEntry(ChatLog, data)  
                     else:
                         # remove the socket that's broken    
                         if sock in SOCKET_LIST:
                             SOCKET_LIST.remove(sock)
 
                         # at this stage, no data means probably the connection has been broken
-                        broadcast(server_socket, sock, "Client (%s, %s) is offline\n" % addr) 
+                        broadcast(s, sock, "Client (%s, %s) is offline\n" % addr) 
+                        # LoadConnectionInfo(ChatLog, "Client "+ str(addr) + "is offline")
 
                 # exception 
                 except:
-                    broadcast(server_socket, sock, "Client (%s, %s) is offline\n" % addr)
+                    broadcast(s, sock, "Client (%s, %s) is offline\n" % addr)
+                    # LoadConnectionInfo(ChatLog, "Client "+ str(addr) + "is offline")
                     continue
 
-    server_socket.close()
-    
+    s.close()
+
 # broadcast chat messages to all connected clients
 def broadcast (server_socket, sock, message):
-    for socket in SOCKET_LIST:
+    for s in SOCKET_LIST:
         # send the message only to peer
-        if socket != server_socket and socket != sock :
+        if s != server_socket and s != sock :
             try :
-                socket.send(message)
+                LoadConnectionInfo(ChatLog, message)
+                s.send(message)
             except :
                 # broken socket connection
-                socket.close()
+                s.close()
                 # broken socket, remove it
-                if socket in SOCKET_LIST:
-                    SOCKET_LIST.remove(socket)
- 
-if __name__ == "__main__":
+                if s in SOCKET_LIST:
+                    SOCKET_LIST.remove(s)
     
-    #sys.exit(chat_server())
-    try:
-        chat_server()
-    except:
-        print '\nChat Server Stopped by Ctrl + C'
-        
+
+
+base.mainloop()
+
 
